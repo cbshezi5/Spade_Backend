40fda899c4c491c0b6d68bc58c1346bd59d3e021
Cebolenkosi Shezi <cbshezi5@gmail.com>
2022-06-04 23:23:19 +0200

    bus

--- contexts/Student/Bus.js
+++ contexts/Student/Bus.js
@@ -21,7 +21,7 @@ Router.get('/Student/GetBooked', (req, res, next) => {
 
     if (req.query.id) {
         
-            mariadb.query('SELECT `Busid`,`Studentid`,`From`,`To`,`Status`,`Temporally`,TIME_FORMAT(Time, "%H:%i") AS Time,`Date` FROM Bus WHERE Studentid = '+req.query.id+'', (err, rows) => {
+            mariadb.query('SELECT `Busid`,`Studentid`,`From`,`To`,`Status`,`Temporally`,TIME_FORMAT(Time, "%H:%i") AS Time,`Date` FROM Bus WHERE Studentid = '+req.query.id+' AND Status <> "Deleted"', (err, rows) => {
                 if (!err) {
                     res.send({
                         error: false,
@@ -47,7 +47,63 @@ Router.get('/Student/GetBooked', (req, res, next) => {
     }
 });
 
+Router.delete('/Student/GetBooked', (req, res, next) => {
+    if (Object.keys(req.query).length == 0) {
+        res.send({
+            error: true,
+            code: "C001_DEL",
+            message: "query parameters were not found"
+        });
+        return
+    }
 
+    if (req.query.Busid) {
+        mariadb.query(`UPDATE Bus SET Status = 'Deleted' WHERE Busid= ${req.query.Busid}`,(err, rows)=>{
+            if(!err)
+            {
+                res.send({
+                    error: false,
+                    data: rows,
+                });
+                return
+            }
+            else
+            {
+                res.send({
+                    error: true,
+                    code: "C001_DEL",
+                    message: err
+                });
+                return
+            }
+        })
+        
+    }
+
+
+    if(req.query.studentid)
+    {
+            mariadb.query(`UPDATE Bus SET Status = 'Deleted' WHERE Studentid = ${req.query.Studentid}`,(err, rows)=>{
+                if(!err)
+                {
+                    res.send({
+                        error: false,
+                        data: rows,
+                    });
+                    return
+                }
+                else
+                {
+                    res.send({
+                        error: true,
+                        code: "C001_DEL",
+                        message: err
+                    });
+                    return
+                }
+            })
+    }
+});
 
 
 module.exports = Router;
